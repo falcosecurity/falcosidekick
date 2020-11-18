b0c6418a34d02cfbe2c720bdffa59b4e0f5fbb5a
Carlos Panato <ctadeu@gmail.com>
2020-11-18 17:36:12 +0100

    fix alermanager test
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- outputs/alertmanager_test.go
+++ outputs/alertmanager_test.go
@@ -13,10 +13,11 @@ func TestNewAlertmanagerPayload(t *testing.T) {
 	expectedOutput := `[{"labels":{"proc_name":"falcosidekick","rule":"Test rule","source":"falco"},"annotations":{"info":"This is a test from falcosidekick","summary":"Test rule"}}]`
 
 	var f types.FalcoPayload
-	json.Unmarshal([]byte(falcoTestInput), &f)
-	s, _ := json.Marshal(newAlertmanagerPayload(f))
+	require.Nil(t, json.Unmarshal([]byte(falcoTestInput), &f))
+	s, err := json.Marshal(newAlertmanagerPayload(f))
+	require.Nil(t, err)
 
-	var o1, o2 alertmanagerPayload
+	var o1, o2 []alertmanagerPayload
 	require.Nil(t, json.Unmarshal([]byte(expectedOutput), &o1))
 	require.Nil(t, json.Unmarshal(s, &o2))
 
