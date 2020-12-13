873cd8d7b62c182a3109f8d4e2fcb62aa3588554
Carlos Panato <ctadeu@gmail.com>
2020-12-13 15:41:46 +0100

    webhook: add missing statds/dogstastd metrics
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- outputs/webhook.go
+++ outputs/webhook.go
@@ -1,19 +1,27 @@
 package outputs
 
 import (
+	"log"
+
 	"github.com/falcosecurity/falcosidekick/types"
 )
 
 // WebhookPost posts event to Slack
 func (c *Client) WebhookPost(falcopayload types.FalcoPayload) {
+	c.Stats.Webhook.Add(Total, 1)
+
 	err := c.Post(falcopayload)
 	if err != nil {
+		go c.CountMetric(Outputs, 1, []string{"output:webhook", "status:error"})
 		c.Stats.Webhook.Add(Error, 1)
 		c.PromStats.Outputs.With(map[string]string{"destination": "webhook", "status": Error}).Inc()
-	} else {
-		c.Stats.Webhook.Add(OK, 1)
-		c.PromStats.Outputs.With(map[string]string{"destination": "webhook", "status": OK}).Inc()
+		log.Printf("[ERROR] : WebHook - %v\n", err.Error())
+		return
 	}
 
-	c.Stats.Webhook.Add(Total, 1)
+	// Setting the success status
+	go c.CountMetric(Outputs, 1, []string{"output:webhook", "status:ok"})
+	c.Stats.Webhook.Add(OK, 1)
+	c.PromStats.Outputs.With(map[string]string{"destination": "webhook", "status": OK}).Inc()
+	log.Printf("[INFO] : WebHook - Publish OK\n")
 }
