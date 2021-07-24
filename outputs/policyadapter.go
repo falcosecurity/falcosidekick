572cec47a181c013ae138b997149f67269beb38c
anushkamittal20 <anumittal4641@gmail.com>
2021-07-24 22:57:32 +0530

    Initial update logic
    Signed-off-by: anushkamittal20 <anumittal4641@gmail.com>

--- outputs/policyadapter.go
+++ outputs/policyadapter.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/util/retry"
+
 	"github.com/DataDog/datadog-go/statsd"
 	"github.com/anushkamittal20/falcoadapter/pkg/apis/wgpolicyk8s.io/v1alpha2"
 	clusterpolicyreport "github.com/anushkamittal20/falcoadapter/pkg/apis/wgpolicyk8s.io/v1alpha2"
@@ -21,7 +24,7 @@ func NewPolicyReportClient(config *types.Configuration, stats *types.Statistics,
 	if err != nil {
 		restConfig, err = clientcmd.BuildConfigFromFlags("", config.PolicyReport.Kubeconfig)
 		if err != nil {
-			fmt.Printf("unable to load kube config file: %v", err)
+			fmt.Printf("[ERROR] :unable to load kube config file: %v", err)
 		}
 	}
 	clientset, err := kubernetes.NewForConfig(restConfig)
@@ -47,29 +50,56 @@ func NewPolicyReportClient(config *types.Configuration, stats *types.Statistics,
 
 // PolicyReportPost creates Policy Report Resource in Kubernetes
 func (c *Client) PolicyReportCreate(falcopayload types.FalcoPayload) {
-	//to do
-
 	ats := c.Crdclient.Wgpolicyk8sV1alpha2().ClusterPolicyReports()
 	report := &clusterpolicyreport.ClusterPolicyReport{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "dummy-policy-report",
 		},
 		Summary: v1alpha2.PolicyReportSummary{
-			Fail: 1,
+			Fail: 1, //hardcoded for time being; to-do
 		},
 	}
 	report.Results = append(report.Results, newResult(falcopayload))
-	result, err := ats.Create(context.TODO(), report, metav1.CreateOptions{})
-	if err != nil {
-		log.Printf("[ERROR] : %v\n", err)
+
+	_, getErr := ats.Get(context.Background(), report.Name, metav1.GetOptions{})
+	if errors.IsNotFound(getErr) {
+		result, err := ats.Create(context.TODO(), report, metav1.CreateOptions{})
+		if err != nil {
+			log.Printf("[ERROR] : %v\n", err)
+		}
+		fmt.Printf("[INFO] :Created policy-report %q.\n", result.GetObjectMeta().GetName())
+	} else {
+		// Update existing Policy Report
+		fmt.Println("[INFO] :updating policy report...")
+		retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+
+			result, err := ats.Get(context.Background(), report.GetName(), metav1.GetOptions{})
+			if errors.IsNotFound(err) {
+				// This doesnt ever happen even if it is already deleted or not found
+				log.Printf("[ERROR] :%v not found", report.GetName())
+				return nil
+			}
+
+			if err != nil {
+				return err
+			}
+
+			report.SetResourceVersion(result.GetResourceVersion())
+
+			_, updateErr := ats.Update(context.Background(), report, metav1.UpdateOptions{})
+			return updateErr
+		})
+		if retryErr != nil {
+			fmt.Printf("[ERROR] :update failed: %v", retryErr)
+		}
+		fmt.Println("[INFO] :updated policy report...")
 	}
-	fmt.Printf("Created policy-report %q.\n", result.GetObjectMeta().GetName())
 }
 
-//mapping
+//mapping for the policy report
 func newResult(FalcoPayload types.FalcoPayload) *clusterpolicyreport.PolicyReportResult {
 	const PolicyReportSource string = "Falco"
-	var pri string
+	var pri string //initial hardcoded priority bounds
 	if FalcoPayload.Priority > 4 {
 		pri = "high"
 	} else if FalcoPayload.Priority < 3 {
