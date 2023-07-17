7ef194ff3e007a252c40f3973c984d5ddfad7ece
Mario Kahlhofer <mario.kahlhofer@dynatrace.com>
2023-07-17 18:19:32 +0200

    feat(dynatrace): [TR-1498] Enhance Dynatrace output with semantic attributes
    Signed-off-by: Mario Kahlhofer <mario.kahlhofer@dynatrace.com>

--- outputs/dynatrace.go
+++ outputs/dynatrace.go
@@ -2,6 +2,7 @@ package outputs
 
 import (
 	"log"
+	"regexp"
 	"time"
 
 	"github.com/falcosecurity/falcosidekick/types"
@@ -12,13 +13,24 @@ type dtPayload struct {
 }
 
 type dtLogMessage struct {
-	Timestamp   string       `json:"timestamp"`
-	Severity    string       `json:"severity,omitempty"`
-	Content     dtLogContent `json:"content"`
-	SpanID      string       `json:"span_id,omitempty"`
-	Hostname    string       `json:"host.name,omitempty"`
-	LogSource   string       `json:"log.source"`
-	AuditAction string       `json:"audit.action"`
+	Timestamp             string       `json:"timestamp"`
+	EventId               string       `json:"event.id,omitempty"`
+	EventName             string       `json:"event.name,omitempty"`
+	EventProvider         string       `json:"event.provider,omitempty"`
+	Severity              string       `json:"severity,omitempty"`
+	HostName              string       `json:"host.name,omitempty"`
+	LogSource             string       `json:"log.source,omitempty"`
+	Content               dtLogContent `json:"content"`
+	MitreTechnique        string       `json:"mitre.technique,omitempty"`
+	MitreTactic           string       `json:"mitre.tactic,omitempty"`
+	ContainerId           string       `json:"container.id,omitempty"`
+	ContainerName         string       `json:"container.name,omitempty"`
+	ContainerImageName    string       `json:"container.image.name,omitempty"`
+	K8sNamespaceName      string       `json:"k8s.namespace.name,omitempty"`
+	K8sPodName            string       `json:"k8s.pod.name,omitempty"`
+	K8sPodId              string       `json:"k8s.pod.id,omitempty"`
+	ProcessExecutableName string       `json:"process.executable.name,omitempty"`
+	SpanId                string       `json:"span.id,omitempty"`
 }
 
 type dtLogContent struct {
@@ -28,20 +40,61 @@ type dtLogContent struct {
 }
 
 const DynatraceContentType = "application/json; charset=utf-8"
+const DynatraceEventProvider = "Falco"
+
+// match MITRE techniques, e.g. "T1070", and sub-techniques, e.g. "T1055.008"
+var MitreTechniqueRegEx = regexp.MustCompile(`T\d+\.?\d*`)
+
+// match MITRE tactics, e.g. "mitre_execution"
+var MitreTacticRegEx = regexp.MustCompile(`mitre_\w+`)
 
 func newDynatracePayload(falcopayload types.FalcoPayload) dtPayload {
 	message := dtLogMessage{
-		Timestamp: falcopayload.Time.Format(time.RFC3339),
-		Severity:  falcopayload.Priority.String(),
+		Timestamp:     falcopayload.Time.Format(time.RFC3339),
+		EventId:       falcopayload.UUID,
+		EventName:     falcopayload.Rule,
+		EventProvider: DynatraceEventProvider,
+		Severity:      falcopayload.Priority.String(),
+		HostName:      falcopayload.Hostname,
+		LogSource:     falcopayload.Source,
 		Content: dtLogContent{
 			Output:       falcopayload.Output,
 			OutputFields: falcopayload.OutputFields,
 			Tags:         falcopayload.Tags,
 		},
-		SpanID:      falcopayload.UUID,
-		Hostname:    falcopayload.Hostname,
-		LogSource:   falcopayload.Source,
-		AuditAction: falcopayload.Rule,
+	}
+
+	// possibly map a few fields to semantic attributes
+	for fcKey, val := range falcopayload.OutputFields {
+		switch fcKey {
+		case "container.id":
+			message.ContainerId = val.(string)
+		case "container.name":
+			message.ContainerName = val.(string)
+		case "container.image.name":
+			message.ContainerImageName = val.(string)
+		case "k8s.namespace.name", "ka.target.namespace":
+			message.K8sNamespaceName = val.(string)
+		case "k8s.pod.name":
+			message.K8sPodName = val.(string)
+		case "k8s.pod.id":
+			message.K8sPodId = val.(string)
+		case "proc.name":
+			message.ProcessExecutableName = val.(string)
+		case "span.id":
+			message.SpanId = val.(string)
+		default:
+			continue
+		}
+	}
+
+	// map tags to MITRE technique and tactic
+	for _, fcTag := range falcopayload.Tags {
+		if MitreTechniqueRegEx.MatchString(fcTag) {
+			message.MitreTechnique = fcTag
+		} else if MitreTacticRegEx.MatchString(fcTag) {
+			message.MitreTactic = fcTag
+		}
 	}
 
 	return dtPayload{Payload: []dtLogMessage{message}}
