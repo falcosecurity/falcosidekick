fc427fc8de87f7dcacb7ecdc57ad5b4a7b4351fc
Mostafa Moradian <mostafamoradian0@gmail.com>
2023-08-10 13:28:36 +0200

    Fix message format

--- outputs/otlp.go
+++ outputs/otlp.go
@@ -2,16 +2,211 @@ package outputs
 
 import (
 	"crypto/rand"
-	"encoding/json"
 	"fmt"
 	"log"
 	"math/big"
 	"strings"
-	"time"
 
 	"github.com/falcosecurity/falcosidekick/types"
 	"go.opentelemetry.io/otel/trace"
 )
+
+type StringValue struct {
+	Value string `json:"stringValue,omitempty"`
+}
+
+type Attribute struct {
+	Key   string      `json:"key,omitempty"`
+	Value StringValue `json:"value,omitempty"`
+}
+
+type Event struct {
+	Name                   string      `json:"name,omitempty"`
+	Timestamp              int64       `json:"timeUnixNano,omitempty"`
+	Attributes             []Attribute `json:"attributes,omitempty"`
+	DroppedAttributesCount uint32      `json:"droppedAttributesCount,omitempty"`
+}
+
+type Link struct {
+	TraceID                trace.TraceID    `json:"traceId,omitempty"`
+	SpanID                 trace.SpanID     `json:"spanId,omitempty"`
+	TraceState             trace.TraceState `json:"traceState,omitempty"`
+	Attributes             []Attribute      `json:"attributes,omitempty"`
+	DroppedAttributesCount uint32           `json:"droppedAttributesCount,omitempty"`
+}
+
+type Status struct {
+	Code    int32  `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+type Span struct {
+	TraceID                trace.TraceID    `json:"traceId,omitempty"`
+	SpanID                 trace.SpanID     `json:"spanId,omitempty"`
+	TraceState             trace.TraceState `json:"traceState,omitempty"`
+	Kind                   trace.SpanKind   `json:"kind,omitempty"`
+	ParentSpanID           *trace.TraceID   `json:"parentSpanId,omitempty"`
+	Name                   string           `json:"name,omitempty"`
+	StartTime              int64            `json:"startTimeUnixNano,omitempty"`
+	EndTime                int64            `json:"endTimeUnixNano,omitempty"`
+	Attributes             []Attribute      `json:"attributes,omitempty"`
+	DroppedAttributesCount uint32           `json:"droppedAttributesCount,omitempty"`
+	Events                 []Event          `json:"events,omitempty"`
+	Links                  []Link           `json:"links,omitempty"`
+	DroppedLinksCount      uint32           `json:"droppedLinksCount,omitempty"`
+	Status                 Status           `json:"status,omitempty"`
+}
+
+type Resource struct {
+	Attributes []Attribute `json:"attributes,omitempty"`
+}
+
+type ResourceSpan struct {
+	Resource   Resource    `json:"resource,omitempty"`
+	ScopeSpans []ScopeSpan `json:"scopeSpans,omitempty"`
+	SchemaURL  string      `json:"schemaUrl,omitempty"`
+}
+
+type Scope struct {
+	Name       string      `json:"name,omitempty"`
+	Version    string      `json:"version,omitempty"`
+	Attributes []Attribute `json:"attributes,omitempty"`
+}
+
+type ScopeSpan struct {
+	Scope     Scope  `json:"scope,omitempty"`
+	Spans     []Span `json:"spans,omitempty"`
+	SchemaURL string `json:"schemaUrl,omitempty"`
+}
+
+type Trace struct {
+	ResourceSpans []ResourceSpan `json:"resourceSpans,omitempty"`
+}
+
+// newTrace returns a new Trace object.
+func newTrace(falcopayload types.FalcoPayload) *Trace {
+	_, exists := falcopayload.OutputFields["container.id"]
+	if !exists {
+		log.Printf("Error getting container id from output fields")
+		return nil
+	}
+	containerID, ok := falcopayload.OutputFields["container.id"].(string)
+	if !ok {
+		log.Printf("Error converting container id to string")
+		return nil
+	}
+
+	// https://www.w3.org/TR/trace-context/#trace-id
+	traceId, err := generateTraceID(containerID)
+	if err != nil {
+		log.Printf("Error generating trace id: %v", err)
+		return nil
+	}
+
+	spanId, err := generateSpanID("")
+	if err != nil {
+		log.Printf("Error generating span id: %v", err)
+		return nil
+	}
+
+	span := Span{
+		Name:         falcopayload.Rule,
+		TraceID:      traceId,
+		SpanID:       spanId,
+		ParentSpanID: nil,
+		TraceState:   trace.TraceState{},
+		StartTime:    falcopayload.Time.UnixNano(),
+		EndTime:      falcopayload.Time.UnixNano(),
+		Kind:         trace.SpanKindServer,
+	}
+	span.Attributes = []Attribute{}
+
+	evtAttribs := []Attribute{
+		{
+			Key:   "uuid",
+			Value: StringValue{Value: falcopayload.UUID},
+		},
+		{
+			Key:   "source",
+			Value: StringValue{Value: falcopayload.Source},
+		},
+		{
+			Key:   "priority",
+			Value: StringValue{Value: falcopayload.Priority.String()},
+		},
+		{
+			Key:   "rule",
+			Value: StringValue{Value: falcopayload.Rule},
+		},
+		{
+			Key: "output",
+			// This is the full log line, which can be further
+			// parsed and broken down into individual fields.
+			Value: StringValue{Value: falcopayload.Output},
+		},
+		{
+			Key:   "hostname",
+			Value: StringValue{Value: falcopayload.Hostname},
+		},
+		{
+			Key:   "tags",
+			Value: StringValue{Value: strings.Join(falcopayload.Tags, ",")},
+		},
+	}
+	for k, v := range falcopayload.OutputFields {
+		evtAttribs = append(evtAttribs,
+			Attribute{Key: k, Value: StringValue{Value: fmt.Sprintf("%v", v)}})
+	}
+
+	span.Events = []Event{
+		{
+			Name:       "falcosidekick.otlp",
+			Timestamp:  falcopayload.Time.UnixNano(),
+			Attributes: evtAttribs,
+		},
+	}
+
+	trace := Trace{
+		ResourceSpans: []ResourceSpan{
+			{
+				Resource: Resource{
+					Attributes: []Attribute{
+						{
+							Key:   "service.name",
+							Value: StringValue{Value: "falcosidekick.otlp"},
+						},
+					},
+				},
+				ScopeSpans: []ScopeSpan{
+					{
+						Scope: Scope{
+							Name:    "falcosidekick-otlp",
+							Version: "1.0.0",
+						},
+						Spans: []Span{span},
+					},
+				},
+			},
+		},
+	}
+
+	log.Print("OTLP payload generated successfully")
+
+	return &trace
+}
+
+func (c *Client) OTLPPost(falcopayload types.FalcoPayload) {
+	trace := newTrace(falcopayload)
+	if trace == nil {
+		log.Printf("Error generating trace")
+		return
+	}
+	err := c.Post(trace)
+	if err != nil {
+		log.Printf("Error sending trace to OTLP endpoint: %v", err)
+		return
+	}
+}
 
 func generateTraceID(containerID string) (trace.TraceID, error) {
 	if containerID == "" {
@@ -48,137 +243,4 @@ func generateSpanID(containerID string) (trace.SpanID, error) {
 		containerID = "0" + containerID
 	}
 	return trace.SpanIDFromHex(containerID)
-}
-
-type Context struct {
-	TraceID trace.TraceID `json:"trace_id,omitempty"`
-	SpanID  trace.SpanID  `json:"span_id,omitempty"`
-}
-
-type Attribute struct {
-	Key   string `json:"key,omitempty"`
-	Value string `json:"value,omitempty"`
-}
-
-type Event struct {
-	Name       string      `json:"name,omitempty"`
-	Timestamp  string      `json:"timestamp,omitempty"`
-	Attributes []Attribute `json:"attributes,omitempty"`
-}
-
-type Trace struct {
-	Name       string         `json:"name,omitempty"`
-	Context    Context        `json:"context,omitempty"`
-	ParentID   *trace.TraceID `json:"parent_id,omitempty"`
-	StartTime  string         `json:"start_time,omitempty"`
-	EndTime    string         `json:"end_time,omitempty"`
-	Attributes []Attribute    `json:"attributes,omitempty"`
-	Events     []Event        `json:"events,omitempty"`
-}
-
-// newTrace returns a new Trace object.
-func newTrace(falcopayload types.FalcoPayload) *Trace {
-	_, exists := falcopayload.OutputFields["container.id"]
-	if !exists {
-		log.Printf("Error getting container id from output fields")
-		return nil
-	}
-	containerID, ok := falcopayload.OutputFields["container.id"].(string)
-	if !ok {
-		log.Printf("Error converting container id to string")
-		return nil
-	}
-
-	// https://www.w3.org/TR/trace-context/#trace-id
-	traceId, err := generateTraceID(containerID)
-	if err != nil {
-		log.Printf("Error generating trace id: %v", err)
-		return nil
-	}
-
-	spanId, err := generateSpanID("")
-	if err != nil {
-		log.Printf("Error generating span id: %v", err)
-		return nil
-	}
-
-	trace := Trace{
-		Name:      "falco",
-		Context:   Context{TraceID: traceId, SpanID: spanId},
-		ParentID:  nil,
-		StartTime: falcopayload.Time.Format(time.RFC3339),
-		EndTime:   falcopayload.Time.Format(time.RFC3339),
-	}
-	trace.Attributes = []Attribute{}
-
-	evtAttribs := []Attribute{
-		{
-			Key:   "uuid",
-			Value: falcopayload.UUID,
-		},
-		{
-			Key:   "source",
-			Value: falcopayload.Source,
-		},
-		{
-			Key:   "priority",
-			Value: falcopayload.Priority.String(),
-		},
-		{
-			Key:   "rule",
-			Value: falcopayload.Rule,
-		},
-		{
-			Key: "output",
-			// This is the full log line, which can be further
-			// parsed and broken down into individual fields.
-			Value: falcopayload.Output,
-		},
-		{
-			Key:   "hostname",
-			Value: falcopayload.Hostname,
-		},
-		{
-			Key:   "tags",
-			Value: strings.Join(falcopayload.Tags, ","),
-		},
-	}
-	for k, v := range falcopayload.OutputFields {
-		evtAttribs = append(evtAttribs, Attribute{Key: k, Value: fmt.Sprintf("%v", v)})
-	}
-
-	events := []Event{
-		{
-			"falcosidekick.otlp",
-			falcopayload.Time.Format(time.RFC3339),
-			evtAttribs,
-		},
-	}
-	trace.Events = events
-
-	log.Print("OTLP payload generated successfully")
-
-	return &trace
-}
-
-func marshalJSON(trace *Trace) ([]byte, error) {
-	result, err := json.Marshal(trace)
-	if err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-func (c *Client) OTLPPost(falcopayload types.FalcoPayload) {
-	trace := newTrace(falcopayload)
-	if trace == nil {
-		log.Printf("Error generating trace")
-		return
-	}
-	// NB: no need to marshalJSON(trace), as c.Post() takes care of that
-	err := c.Post(trace)
-	if err != nil {
-		log.Printf("Error sending trace to OTLP endpoint: %v", err)
-		return
-	}
 }
