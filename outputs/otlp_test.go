fe3db5c81b5f30891e11102c98a3b185997781bc
JuanJo Ciarlante <juanjosec@gmail.com>
2023-08-18 21:03:14 -0300

    refactor+improve otlp_test.go
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- outputs/otlp_test.go
+++ outputs/otlp_test.go
@@ -5,16 +5,18 @@ import (
 	"testing"
 	"time"
 
-	"github.com/falcosecurity/falcosidekick/types"
 	"github.com/google/uuid"
-	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
+
+	"github.com/falcosecurity/falcosidekick/types"
+	"github.com/samber/lo"
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
 
+// Need to mock three interfaces: TracerProvider, Tracer, Span
 type MockTracerProvider struct{}
 type MockTracer struct{}
 type MockSpan struct {
@@ -26,10 +28,12 @@ type MockSpan struct {
 	attributes map[attribute.Key]attribute.Value
 }
 
+// TracerProvider interface {
 func (*MockTracerProvider) Tracer(string, ...trace.TracerOption) trace.Tracer {
 	return &MockTracer{}
 }
 
+// Tracer interface
 func (*MockTracer) Start(ctx context.Context, name string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
 	return ctx, &MockSpan{
 		ctx:        ctx,
@@ -39,23 +43,26 @@ func (*MockTracer) Start(ctx context.Context, name string, opts ...trace.SpanSta
 	}
 }
 
-func (*MockSpan) AddEvent(string, ...trace.EventOption) {}
+// Span interface
+func (*MockSpan) AddEvent(string, ...trace.EventOption)            {}
+func (*MockSpan) IsRecording() bool                                { return true }
+func (*MockSpan) RecordError(err error, opts ...trace.EventOption) {}
+func (*MockSpan) SetName(name string)                              {}
+func (*MockSpan) SetStatus(code codes.Code, description string)    {}
+
+func (*MockSpan) TracerProvider() trace.TracerProvider { return &MockTracerProvider{} }
+
 func (m *MockSpan) End(opts ...trace.SpanEndOption) {
 	m.endOpts = opts
 }
-func (*MockSpan) IsRecording() bool                                { return true }
-func (*MockSpan) RecordError(err error, opts ...trace.EventOption) {}
 func (m *MockSpan) SetAttributes(kv ...attribute.KeyValue) {
 	for _, k := range kv {
 		m.attributes[k.Key] = k.Value
 	}
 }
-func (*MockSpan) SetName(name string)                           {}
-func (*MockSpan) SetStatus(code codes.Code, description string) {}
 func (m *MockSpan) SpanContext() trace.SpanContext {
 	return trace.SpanContextFromContext(m.ctx)
 }
-func (*MockSpan) TracerProvider() trace.TracerProvider { return &MockTracerProvider{} }
 
 func MockGetTracerProvider() trace.TracerProvider {
 	return &MockTracerProvider{}
@@ -76,32 +83,62 @@ func endOptIn(opt trace.SpanEndOption, opts []trace.SpanEndOption) bool {
 
 func TestOtlpNewTrace(t *testing.T) {
 	getTracerProvider = MockGetTracerProvider
-	containerID := "42"
-	uuidStr := uuid.New().String()
-	durationMs := int64(100)
-	startTime := time.Now()
-	endTime := startTime.Add(time.Millisecond * time.Duration(durationMs))
-	optStartTime := trace.WithTimestamp(startTime)
-	optEndTime := trace.WithTimestamp(endTime)
 
-	fp := &types.FalcoPayload{
-		Time: startTime,
-		Rule: "Mock Rule",
-		OutputFields: map[string]interface{}{
-			"priority":     "info",
-			"uuid":         uuidStr,
-			"source":       "falco",
-			"container.id": containerID,
-			"hostname":     "myhost",
-			"output":       "Hook this Mock!",
+	cases := []struct {
+		payload    *types.FalcoPayload
+		startTime  time.Time
+		durationMs int64
+		expectSpan bool
+	}{
+		{
+			durationMs: 100,
+			startTime:  time.Now(),
+			payload: &types.FalcoPayload{
+				Rule: "Mock Rule#1",
+				OutputFields: map[string]interface{}{
+					"priority":     "info",
+					"uuid":         uuid.New().String(),
+					"source":       "falco",
+					"container.id": "42",
+					"hostname":     "myhost",
+					"output":       "Hook this Mock!",
+				},
+			},
+			expectSpan: true,
+		},
+		{
+			payload: &types.FalcoPayload{
+				Rule: "Mock Rule#2",
+				OutputFields: map[string]interface{}{
+					"container.id": "not-Hex",
+				},
+			},
+			expectSpan: false,
 		},
 	}
-	span := newTrace(*fp, durationMs)
-	require.Equal(t, startOptIn(optStartTime, (*span).(*MockSpan).startOpts), true)
-	require.Equal(t, endOptIn(optEndTime, (*span).(*MockSpan).endOpts), true)
-	for k, v := range fp.OutputFields {
-		require.Equal(t, attribute.StringValue(v.(string)), (*span).(*MockSpan).attributes[attribute.Key(k)])
+	for _, c := range cases {
+		c.payload.Time = c.startTime
+
+		// Test newTrace()
+		span := newTrace(*c.payload, c.durationMs)
+		if !c.expectSpan {
+			require.Nil(t, span)
+			continue
+		}
+
+		// Verify SpanStartOption and SpanEndOption timestamps
+		optStartTime := trace.WithTimestamp(c.startTime)
+		optEndTime := trace.WithTimestamp(c.startTime.Add(time.Millisecond * time.Duration(c.durationMs)))
+		require.Equal(t, startOptIn(optStartTime, (*span).(*MockSpan).startOpts), true, c.payload.Rule)
+		require.Equal(t, endOptIn(optEndTime, (*span).(*MockSpan).endOpts), true, c.payload.Rule)
+
+		// Verify span attributes
+		for k, v := range c.payload.OutputFields {
+			require.Equal(t, attribute.StringValue(v.(string)), (*span).(*MockSpan).attributes[attribute.Key(k)], c.payload.Rule)
+		}
+
+		// Verify traceID
+		spanTraceID, _ := generateTraceID(c.payload.OutputFields["container.id"].(string))
+		require.Equal(t, (*span).(*MockSpan).SpanContext().TraceID(), spanTraceID, c.payload.Rule)
 	}
-	spanTraceID, _ := generateTraceID(containerID)
-	require.Equal(t, (*span).(*MockSpan).SpanContext().TraceID(), spanTraceID)
 }
