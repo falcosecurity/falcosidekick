a576ddf0b6a9c7290af51a2e41888716af3343cc
Jorge Carpio <carpioldc@gmail.com>
2023-08-29 15:58:40 +0200

    Fix G402: TLS MinVersion too low
    Signed-off-by: Jorge Carpio <carpioldc@gmail.com>

--- outputs/client.go
+++ outputs/client.go
@@ -194,10 +194,10 @@ func (c *Client) sendRequest(method string, payload interface{}) error {
 	customTransport := http.DefaultTransport.(*http.Transport).Clone()
 
 	if customTransport.TLSClientConfig == nil {
-		customTransport.TLSClientConfig = &tls.Config{}
+		customTransport.TLSClientConfig = &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		}
 	}
-
-	customTransport.TLSClientConfig.MinVersion = tls.VersionTLS12
 
 	if customTransport.TLSClientConfig.RootCAs == nil {
 		pool, err := x509.SystemCertPool()
